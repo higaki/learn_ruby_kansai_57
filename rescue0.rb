5721fa07143643c6d13f01dd31ae01ccac675f54
HIGAKI Masaru <mas.higa@gmail.com>
2013-03-31 22:32:53 +0900

    refactor

--- rescue0.rb
+++ rescue0.rb
@@ -1,7 +1,7 @@
 #! /usr/bin/env ruby
 require './factorial'
 
-def factorial(n)
+def fact(n)
   n.factorial
 rescue ArgumentError
   "1以上の整数を指定してください"
@@ -9,9 +9,9 @@ rescue NoMethodError
   "整数を指定してください"
 end
 
-factorial 3     # => 6
-factorial 4     # => 24
-factorial 5     # => 120
-factorial 0     # => "1以上の整数を指定してください"
-factorial 2.5   # => "整数を指定してください"
-factorial "2"   # => "整数を指定してください"
+fact 3      # => 6
+fact 4      # => 24
+fact 5      # => 120
+fact 0      # => "1以上の整数を指定してください"
+fact 2.5    # => "整数を指定してください"
+fact "2"    # => "整数を指定してください"
